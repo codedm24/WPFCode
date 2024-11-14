b41630388cc88cc6a241587481f25c5753708bcc
codedm24 <mithun.adroit@gmail.com>
2024-11-14 22:34:07 +0530

    WPF triggers

---
+++ WpfAppConceptCheck/WpfAppConceptCheck/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppConceptCheck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _userInput;
+        public string UserInput { get => _userInput; set  
+                { 
+                _userInput = value; OnPropertyChanged("UserInput"); 
+            } 
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Button).Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            (sender as Button).Background = new SolidColorBrush(Colors.Violet);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
